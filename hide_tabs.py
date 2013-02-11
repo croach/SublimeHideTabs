3e00fc25bb721c4d9b2d342a6b6c0e1b44da72aa
Christopher Roach <vthakr@gmail.com>
2013-02-10 22:01:31 -0800

    Refactoring the code and adding comments

--- hide_tabs.py
+++ hide_tabs.py
@@ -1,63 +1,79 @@
+"""
+This plugin manages hiding/showing tabs according to the number of open views
+"""
+
 import sys
 from collections import defaultdict
 
 import sublime
 import sublime_plugin
 
-PYTHON_3 = 0x030000F0
+
+def sublime_text_3():
+    """Returns True if this is Sublime Text 3
+    """
+    try:
+        return int(sublime.version()) >= 3000
+    except ValueError:
+        return sys.hexversion >= 0x030000F0
+
 
 class HideTabsEventListener(sublime_plugin.EventListener):
 
-    _show_tabs = defaultdict(bool)
+    # Holds the tabs visibility for each of the currently open windows
+    _tabs_visibility = defaultdict(bool)
 
-    if sys.hexversion > PYTHON_3:
+    # Use the async versions of the event handlers available in Sublime Text 3
+    if sublime_text_3():
         def on_new_async(self, view):
-            self.toggle_tabs()
+            self.update_tabs_visibility()
+
+        def on_clone_async(self, view):
+            self.update_tabs_visibility()
+
+        def on_activated_async(self, view):
+            self.update_tabs_visibility()
+
+        def on_deactivated_async(self, view):
+            self.update_tabs_visibility()
+
+    # Otherwise, just use the synchronous versions if this is Sublime Text 2
     else:
         def on_new(self, view):
-            self.toggle_tabs()
+            self.update_tabs_visibility()
 
-    def on_close(self, view):
-        self.toggle_tabs()
-
-    if sys.hexversion > PYTHON_3:
-        def on_clone_async(self, view):
-            self.toggle_tabs()
-    else:
         def on_clone(self, view):
-            self.toggle_tabs()
+            self.update_tabs_visibility()
 
-    if sys.hexversion > PYTHON_3:
-        def on_activated_async(self, view):
-            self.toggle_tabs()
-    else:
         def on_activated(self, view):
-            self.toggle_tabs()
+            self.update_tabs_visibility()
 
-    if sys.hexversion > PYTHON_3:
-        def on_deactivated_async(self, view):
-            self.toggle_tabs()
-    else:
         def on_deactivated(self, view):
-            self.toggle_tabs()
+            self.update_tabs_visibility()
 
-    def toggle_tabs(self):
+    # There is no async version of on_close, so just use the regular one
+    def on_close(self, view):
+        self.update_tabs_visibility()
+
+    def update_tabs_visibility(self):
+        """Show/hide tabs based on the number of views in the active window
+        """
         window = sublime.active_window()
         if not window:
             return
 
         num_of_views = len(window.views())
         if num_of_views > 1:
-            self.show_tabs = True
+            self.tabs_visible = True
         elif num_of_views < 2:
-            self.show_tabs = False
+            self.tabs_visible = False
 
-    def _get_show_tabs(self):
+    def _get_tabs_visible(self):
         window = sublime.active_window()
-        return HideTabsEventListener._show_tabs[window.id()]
-    def _set_show_tabs(self, show):
+        return HideTabsEventListener._tabs_visibility[window.id()]
+    def _set_tabs_visible(self, visible):
         window = sublime.active_window()
-        if self.show_tabs != show:
+        if self.tabs_visible != visible:
             window.run_command('toggle_tabs')
-        HideTabsEventListener._show_tabs[window.id()] = show
-    show_tabs = property(_get_show_tabs, _set_show_tabs)
+        HideTabsEventListener._tabs_visibility[window.id()] = visible
+    tabs_visible = property(_get_tabs_visible, _set_tabs_visible)
